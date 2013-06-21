4ee54e1e86043d31f91ce13fd9b62549e3bfd224
Rado Buransky <radoburansky@gmail.com>
2013-06-20 23:30:45 -0700

    PostgreSQL initialized

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.db._
+import play.api.Play.current
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("xxx"))
+    var cat = "xx"
+    DB.withConnection(conn => {
+    	cat = conn.getCatalog()
+    })
+    Ok(views.html.index(cat))
   }
   
 }
