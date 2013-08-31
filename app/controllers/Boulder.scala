06844bbde4405364428b82dab9d4c70c42d606b5
Rado Buransky <radoburansky@gmail.com>
2013-08-31 15:46:43 -0700

    Jpeg resize, quality set

--- app/controllers/Boulder.scala
+++ app/controllers/Boulder.scala
@@ -8,10 +8,12 @@ import views._
 import java.text.SimpleDateFormat
 import play.api.libs.iteratee.Enumerator
 import java.awt.image.BufferedImage
-import javax.imageio.ImageIO;
+import javax.imageio.ImageIO
 import org.imgscalr.Scalr
 import scala.concurrent._
 import ExecutionContext.Implicits.global
+import javax.imageio.ImageWriteParam
+import javax.imageio.IIOImage
 
 object Boulder extends Controller {
   def upload = Action(parse.multipartFormData) {
@@ -21,24 +23,49 @@ object Boulder extends Controller {
           // Check photo type
           if (checkIfPhoto(photo.contentType.get)) {
             Async {
-              	future {
-              	  	// Read and resize image
-		            val rsImg = Scalr.resize(ImageIO.read(photo.ref.file), 150)
-		
-		            // Write file
-		            ImageIO.write(rsImg, "jpg", photo.ref.file)
-		
-		            //insert photo
-		            //FileModel.insertFile(photo.ref.file, photo.contentType.orNull)
-		            
+              future {
+                try {
+									// Read and resize image
+									val rsImg = Scalr.resize(ImageIO.read(photo.ref.file), 800)
+									
+									val jpgWriter = ImageIO.getImageWritersByFormatName("jpg").next();
+									try {
+										// Set quality
+										val param = jpgWriter.getDefaultWriteParam();
+										param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+										param.setCompressionQuality(0.5f);
+										
+										// Write to temporary file
+										photo.ref.file.delete()
+										val outputStream = ImageIO.createImageOutputStream(photo.ref.file)
+										try {
+											jpgWriter.setOutput(outputStream);		            
+										  jpgWriter.write(null, new IIOImage(rsImg, null, null), param);
+										}
+										finally {
+										  outputStream.close();
+										}
+									}
+									finally {
+									  jpgWriter.dispose();
+									}
+                }
+                catch {
+                  case ex: Throwable =>	{
+                    Logger.error("Cannot process uploaded photo!", ex)
+                    BadRequest("Cannot process uploaded photo!")
+                  }
+                }
+		            		            
 		            Ok		            
-              	}
+            	}
             }
           }
-          else
+          else {
+            Logger.error("Format not supported! [" + photo.contentType.get + "]")
             BadRequest("Format not supported!")
+          }
         }
-
         case None => BadRequest("No photo uploaded!")
       }
   }
